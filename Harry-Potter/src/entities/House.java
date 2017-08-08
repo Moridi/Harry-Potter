c780923099e20fb06384ad4f9ed99b06aa81d796
moridi <m.moridi.2009@gmail.com>
2017-08-08 10:27:17 +0430

    Adding House class

---
+++ Harry-Potter/src/entities/House.java
@@ -0,0 +1,15 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Vector;
+import java.util.HashMap;
+import java.util.Map;
+
+public class House {
+	private String name;
+	private School school;
+	private Vector<Student> students;
+	private Professor HeadTeacher;
+	private ArrayList<String> qualities;
+	private Map<Integer, Student> prefects;
+}
