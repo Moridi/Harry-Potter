6899280dcb4e0f79ff4bedc55a310a5c49841e81
moridi <m.moridi.2009@gmail.com>
2017-08-11 20:24:50 +0430

    Student and FinishedCourse class added

--- Harry-Potter/src/entities/House.java
+++ Harry-Potter/src/entities/House.java
@@ -12,4 +12,41 @@ public class House {
 	private Professor HeadTeacher;
 	private ArrayList<String> qualities;
 	private Map<Integer, Student> prefects;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public School getSchool() {
+		return school;
+	}
+	public void setSchool(School school) {
+		this.school = school;
+	}
+	public Vector<Student> getStudents() {
+		return students;
+	}
+	public void setStudents(Vector<Student> students) {
+		this.students = students;
+	}
+	public Professor getHeadTeacher() {
+		return HeadTeacher;
+	}
+	public void setHeadTeacher(Professor headTeacher) {
+		HeadTeacher = headTeacher;
+	}
+	public ArrayList<String> getQualities() {
+		return qualities;
+	}
+	public void setQualities(ArrayList<String> qualities) {
+		this.qualities = qualities;
+	}
+	public Map<Integer, Student> getPrefects() {
+		return prefects;
+	}
+	public void setPrefects(Map<Integer, Student> prefects) {
+		this.prefects = prefects;
+	}
 }
